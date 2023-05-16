6cfc810f11c308e822234b8fddcbd09c8d6dd335
EugeniuPeruzzi <jaka1997@live.it>
2023-05-16 14:40:43 +0200

    corected fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const const_number = '21';
+
 let user_name = prompt('Quale e il tuo nome di nascita?');
 
 let user_surname = prompt('Quale e il tuo cognome?');
@@ -5,7 +7,7 @@ let user_surname = prompt('Quale e il tuo cognome?');
 let user_favorite_color = prompt('Quale e il tuo colore preferito?');
 
 
-let pwd_user = (`${user_name}${user_surname}${user_favorite_color}${21}`);
+let pwd_user = (`${user_name}${user_surname}${user_favorite_color}${const_number}`);
 
 
 document.getElementById('pwd_user').innerHTML =`${pwd_user}`;
